c5d8e12dcd2b036f5a139907e714b359d04e3d81
CultureBMo <benjaminjmorris@hotmail.com>
2014-08-24 08:00:56 +0100

    Added Opacity slider

--- OptionsForm.Designer.cs
+++ OptionsForm.Designer.cs
@@ -29,50 +29,18 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionsForm));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.colourTextBox = new System.Windows.Forms.TextBox();
-            this.colorDialog = new System.Windows.Forms.ColorDialog();
             this.chooseButton = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.colourTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.opacityTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.opacityTrackBar)).BeginInit();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.chooseButton);
-            this.groupBox1.Controls.Add(this.colourTextBox);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(284, 261);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Options";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Colour:";
-            // 
-            // colourTextBox
-            // 
-            this.colourTextBox.Location = new System.Drawing.Point(58, 27);
-            this.colourTextBox.Name = "colourTextBox";
-            this.colourTextBox.Size = new System.Drawing.Size(100, 20);
-            this.colourTextBox.TabIndex = 1;
-            // 
-            // colorDialog
-            // 
-            this.colorDialog.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             // 
             // chooseButton
             // 
-            this.chooseButton.Location = new System.Drawing.Point(164, 25);
+            this.chooseButton.Location = new System.Drawing.Point(170, 12);
             this.chooseButton.Name = "chooseButton";
             this.chooseButton.Size = new System.Drawing.Size(75, 23);
             this.chooseButton.TabIndex = 1;
@@ -80,28 +48,70 @@
             this.chooseButton.UseVisualStyleBackColor = true;
             this.chooseButton.Click += new System.EventHandler(this.ChooseButton_Click);
             // 
+            // colourTextBox
+            // 
+            this.colourTextBox.Location = new System.Drawing.Point(64, 14);
+            this.colourTextBox.Name = "colourTextBox";
+            this.colourTextBox.Size = new System.Drawing.Size(100, 20);
+            this.colourTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Colour:";
+            // 
+            // colorDialog
+            // 
+            this.colorDialog.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Opacity:";
+            // 
+            // opacityTrackBar
+            // 
+            this.opacityTrackBar.Location = new System.Drawing.Point(64, 50);
+            this.opacityTrackBar.Name = "opacityTrackBar";
+            this.opacityTrackBar.Size = new System.Drawing.Size(181, 45);
+            this.opacityTrackBar.TabIndex = 3;
+            // 
             // OptionsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(268, 100);
+            this.Controls.Add(this.opacityTrackBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chooseButton);
+            this.Controls.Add(this.colourTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "OptionsForm";
-            this.Text = "ProLexia";
+            this.Text = "ProLexia Options";
             this.Load += new System.EventHandler(this.OptionsForm_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.opacityTrackBar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button chooseButton;
         private System.Windows.Forms.TextBox colourTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar opacityTrackBar;
     }
 }
